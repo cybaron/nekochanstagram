a6298d1b70dbe285c07beceaea74d11223accdf9
Kazuki Asano <cybaron@gmail.com>
2012-02-26 01:06:26 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nekochanstagram::Application.config.secret_token = '019d76f47635af5b86b9257cb71253769e7b78cd09e67cafdd1a0165764e9d71044d98ec417859917927205148aca0beb4168753dd68bad69868953724e356b8'
