5f9ad51e57dcbbaf1da2a1b5b5d02c0307a0cc58
Kazuki Asano <cybaron@gmail.com>
2012-03-10 19:32:22 +0900

    ワーニング、完了通知処理追加

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,25 +2,32 @@ class ContactsController < ApplicationController
   def index
     if !request.post?
       @contact = Contact.new
+      flash[:notice] = ''
       return
     end
 
     @contact = Contact.new(params[:contact])
     if @contact.valid?
+      # validation OK
       @contact.confirming = 1
+    else
+      # 入力画面に遷移してワーニング表示
+      return
     end
 
-    #cancelボタン押下処理
+    # cancelボタン押下処理
     if params[:cancel]
       @contact.confirming = ''
+      return
     end
 
-    #送信ボタン押下処理
+    # 送信ボタン押下処理
     if params[:send]
-      #メール送信処理
+      # メール送信処理
       mail = ContactMailer.notice(@contact)
       mail.deliver
 
+      flash[:notice] = 'mail send success! thanks.'
       @contact = Contact.new
     end
   end
